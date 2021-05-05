4e535dbd4d35b3d4e5fb77ded8b3eb2f26b0bb0b
AakritiBa <71770614+AakritiBa@users.noreply.github.com>
2021-05-05 19:01:46 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -11,9 +11,14 @@ var bird, slingshot;
 var gameState = "onSling";
 var bg = "sprites/bg1.png";
 var score = 0;
+var birds = [];
 
 function preload() {
     getBackgroundImg();
+    bgImg=loadImage(bg)
+    birdFlySound=loadSound("sounds/bird_flying.mp3")
+    pigSnotSound=loadSound("sounds/pig_snort.mp3")
+    birdSelectSound=loadSound("sounds/bird_select.mp3")
 }
 
 function setup(){
@@ -41,7 +46,14 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(200,50);
+    bird2 = new Bird(150,170);
+    bird3 = new Bird(100,170);
+    bird4 = new Bird(50,170);
 
+    birds.push(bird4)
+    birds.push(bird3)
+    birds.push(bird2)
+    birds.push(bird)
     //log6 = new Log(230,180,80, PI/2);
     slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
@@ -75,15 +87,21 @@ function draw(){
     log5.display();
 
     bird.display();
+    bird2.display();
+    bird3.display();
+    bird4.display();
     platform.display();
     //log6.display();
     slingshot.display();    
 }
 
 function mouseDragged(){
-    //if (gameState!=="launched"){
-        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-    //}
+    if (gameState!=="launched"){
+        Matter.Body.setPosition(birds[birds.length-1].body,{x:mouseX, y:mouseY});
+        Matter.Body.applyForce(birds[birds.length-1],birds[birds.length-1].body.position,{x:5, y:5})
+        birdSelectSound.play()
+        return false
+    }
 }
 
 
@@ -93,11 +111,14 @@ function mouseReleased(){
 }
 
 function keyPressed(){
-    if(keyCode === 32){
-       slingshot.attach(bird.body);
+    if(keyCode === 32 && gameState === "launched"){
+        if(birds.length>=0){
+        Matter.Body.setPosition(birds[birds.length-1].body, {x: 200 , y: 50})
+       slingshot.attach(birds[birds.length-1].body);
+       gameState ="onSling"
     }
+ }
 }
-
 async function getBackgroundImg(){
     var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
     var responseJSON = await response.json();
@@ -114,4 +135,4 @@ async function getBackgroundImg(){
 
     backgroundImg = loadImage(bg);
     console.log(backgroundImg);
-}
+ }
